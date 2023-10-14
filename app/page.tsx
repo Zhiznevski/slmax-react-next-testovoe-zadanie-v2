6a3d81175b94f8d61b05beebaddbad27e82cc9e9
Zhiznevski <w.heisenberggg@gmail.com>
2023-10-14 18:32:44 +0300

    feat: add photo collection on main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,34 @@
-function Home() {
+"use client"
 
+import { api} from "@/utils/api"
+import { useEffect, useState } from "react"
+import { Basic } from "unsplash-js/dist/methods/photos/types";
+
+function Home() {
+  const [photos, setPhotos] = useState<Basic[] | undefined>(undefined)
+  useEffect(() => {
+      api.search.getPhotos({
+      query: 'cats',
+      page: 1,
+      perPage: 10,
+      orientation: "landscape",
+    })
+    .then(photos => {
+        setPhotos(photos.response?.results)
+        console.log(photos.response?.results)
+
+    })
+  }, [])
   return (
+    <>
     <h1>Main page</h1>
+    <ul></ul>
+    {photos?.map(photo => (
+      <img className="img" key={photo.id} src={photo.urls.regular}></img>
+      
+    ))
+    }
+    </>
   )
 }
 
---
+++ utils/api.ts
@@ -0,0 +1,7 @@
+import { createApi } from "unsplash-js"
+
+const accessKey = "Qh3mbY0J2JB09cXA4q3chUeVR3OCc8-f8_u234FELoc"
+
+export const api = createApi({
+    accessKey: accessKey
+})
