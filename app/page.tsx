b0e73c1e943921896ab4554215f1dafb5ac8921b
Zhiznevski <w.heisenberggg@gmail.com>
2023-10-14 22:07:01 +0300

    feat: add pagination to project

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,39 @@
 "use client"
 
 import { api} from "@/utils/api"
+import Pagination from "@mui/material/Pagination";
 import { useEffect, useState } from "react"
 import { Basic } from "unsplash-js/dist/methods/photos/types";
+import Image from 'next/image'
 
 function Home() {
   const [photos, setPhotos] = useState<Basic[] | undefined>(undefined)
+  const [page, setPage] = useState(1);
   useEffect(() => {
       api.search.getPhotos({
-      query: 'cats',
-      page: 1,
+      query: 'french bulldog',
+      page: page,
       perPage: 10,
-      orientation: "landscape",
+      orientation: 'landscape',
+      orderBy: 'relevant',
     })
     .then(photos => {
         setPhotos(photos.response?.results)
         console.log(photos.response?.results)
 
     })
-  }, [])
+  }, [page])
   return (
     <>
     <h1>Main page</h1>
-    <ul></ul>
     {photos?.map(photo => (
-      <img className="img" key={photo.id} src={photo.urls.regular}></img>
+      <Image key={photo.id} src={photo.urls.regular} alt={photo.alt_description || 'Picture'} width={400} height={280} priority/>
       
     ))
     }
+    <Pagination page={page} count={10} variant="outlined" color="primary" onChange={(_, num): void => {
+              setPage(num);
+            }} />
     </>
   )
 }
--- utils/api.ts
+++ utils/api.ts
@@ -1,6 +1,6 @@
 import { createApi } from "unsplash-js"
 
-const accessKey = "Qh3mbY0J2JB09cXA4q3chUeVR3OCc8-f8_u234FELoc"
+const accessKey = "Qh3mbY0J2JB09cXA4q3chUeVR3OCc8-f8_u234FELoc";
 
 export const api = createApi({
     accessKey: accessKey
