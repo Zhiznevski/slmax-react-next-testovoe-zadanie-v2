a5f3d65ec2873906d838fee59998a49ff45d82d9
Zhiznevski <w.heisenberggg@gmail.com>
2023-10-14 15:49:56 +0300

    feat: add basic layout

---
+++ components/Header.tsx
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+import React from 'react'
+
+function Header() {
+  return (
+    <header>
+        <Link href="/">Home</Link>
+        <Link href="login">LogIn</Link>
+    </header>
+  )
+}
+
+export default Header
